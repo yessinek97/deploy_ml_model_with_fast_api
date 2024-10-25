78d2b1f8771a98e0e43fb31610c00f9fb1ba7598
yessinek97 <y.khanfir@instadeep.com>
2024-10-25 17:33:32 +0100

    implement main script for data loading and training

--- starter/train_model.py
+++ starter/train_model.py
@@ -1,10 +1,11 @@
 # Script to train machine learning model.
-
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from ml.data import process_data
 from ml.model import train_model
 
-data_path = "../"
+data_path = "data/census.csv"
+data = pd.read_csv(data_path)
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -24,5 +25,9 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
